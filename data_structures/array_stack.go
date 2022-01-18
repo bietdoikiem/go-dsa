4ff3302533114350e1b5c1b657dcfdc56236d256
bietdoikiem <minhlucky2408@gmail.com>
2022-01-19 00:24:07 +0700

    feat: add stack implemented with array/slice

--- data_structures/array_stack.go
+++ data_structures/array_stack.go
@@ -1,7 +1,5 @@
 package data_structures
 
-import "fmt"
-
 /*
 	Implementation of the Stack data structure using Golang
 	Methods:
@@ -12,11 +10,34 @@ import "fmt"
 */
 
 type ArrayStack[T any] struct {
-	size int
-
-	items []T
+	capacity int
+	size     int
+	top      int
+	items    []T
 }
 
-func PrintHello() {
-	fmt.Println("Hello Data Structures")
+// NewArrayStack function returns a pointer to a newly created Stack using array
+func NewArrayStack[T any](capacity int) *ArrayStack[T] {
+	return &ArrayStack[T]{capacity: capacity, size: 0, top: -1, items: make([]T, capacity)}
+}
+
+// Push pushes the specified item onto the top of the stack
+func (s *ArrayStack[T]) Push(item T) {
+	s.top += 1
+	s.items[s.top] = item
+	s.size += 1
+}
+
+// Pop removes the top item in the stack
+func (s *ArrayStack[T]) Pop() T {
+	popItem := s.items[s.top]
+	s.items = s.items[0:s.top] // Remove last element
+	s.size -= 1
+	s.top -= 1
+	return popItem
+}
+
+// Peek reviews the last added item in the stack
+func (s *ArrayStack[T]) Peek() T {
+	return s.items[s.top]
 }
--- data_structures/array_stack_test.go
+++ data_structures/array_stack_test.go
@@ -1,7 +1,28 @@
 package data_structures
 
-import "testing"
+import (
+	"testing"
 
-func Test_Array_stack(t *testing.T) {
-	PrintHello()
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_array_stack(t *testing.T) {
+	assert := assert.New(t) // Init assertion library
+	stack := NewArrayStack[int](5)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	stack.Push(5)
+	// Assert Push
+	assert.Equal(stack.size, 5, "Stack size should be 5.")
+	assert.Equal(stack.top, 4, "Top item index should be 4.")
+	// Assert Peek
+	assert.Equal(stack.Peek(), stack.items[stack.top], "Peeked item should be the top one in the array.")
+	// Assert Pop
+	topItem := stack.items[stack.top]
+	popItem := stack.Pop()
+	assert.Equal(stack.size, 4, "Stack size should be 4 after popping last item.")
+	assert.Equal(stack.top, 3, "Top item index should be 4 after popping last item.")
+	assert.Equal(popItem, topItem, "Popped item should be equal to the previously top one.")
 }
