c36278038ac3e52a5a228e27e21cd7ed42453ac1
bietdoikiem <minhlucky2408@gmail.com>
2022-01-29 23:14:19 +0700

    feat: add insertion sort (recursive ver)

--- algo/sorts/insertion.go
+++ algo/sorts/insertion.go
@@ -17,3 +17,21 @@ func InsertionSort[T constraints.Ordered](arr []T) {
 		arr[j+1] = temp
 	}
 }
+
+// RecursiveInsertionSort sorts the elements using Insertion Sort
+// algorithm in recursion approach.
+// Time complexity: still O(n^2) - Stable
+func RecursiveInsertionSort[T constraints.Ordered](i int, arr []T) {
+	// Base case: out of bounds index
+	if i <= 0 {
+		return
+	}
+	temp := arr[i]
+	j := i - 1
+	for j >= 0 && temp < arr[j] {
+		RecursiveInsertionSort(i-1, arr)
+		arr[j+1] = arr[j]
+		j -= 1
+	}
+	arr[j+1] = temp
+}
--- algo/sorts/insertion_test.go
+++ algo/sorts/insertion_test.go
@@ -16,3 +16,13 @@ func Test_insertion_sort(t *testing.T) {
 	assert.Equal(7, arr[3], "Last element should be 7.")
 	assert.Equal(2, arr[0], "First element should be 2.")
 }
+
+func Test_recursive_insertion_sort(t *testing.T) {
+	assert := assert.New(t)
+	arr := []int{7, 5, 4, 2, 1}
+	RecursiveInsertionSort(len(arr)-1, arr)
+	Display(arr)
+	// Test assertion
+	assert.Equal(7, arr[4], "Last element should be 7.")
+	assert.Equal(1, arr[0], "First element should be 1.")
+}
