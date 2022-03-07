c3298103388fb86900d6bcf2c787714acad60af2
bietdoikiem <minhlucky2408@gmail.com>
2022-03-07 16:43:59 +0700

    chore: initialized solution for Leetcode No. 15

---
+++ leet/15_3Sum/solution.go
@@ -0,0 +1,10 @@
+package threesum
+
+// threeSum returns list of 3-element subarrays that add up to 0
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	m := make(map[int]int)
+	ans = make([][]int, 1)
+	var t int // Target
+	var diff int
+}
---
+++ leet/15_3Sum/solution_test.go
@@ -0,0 +1,26 @@
+package threesum
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+/*
++Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
++Example 2:
+Input: nums = []
+Output: []
+
++Example 3:
+Input: nums = [0]
+Output: []
+*/
+
+func Test_threeSum_1(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(1, 1)
+}
